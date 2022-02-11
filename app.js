bbae4e23dd1c0c7adb5c542555b4050f63efe42d
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-02-11 09:57:43 -0600

    Added initial js and root variable to css+

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //do work
+  const container = document.getElementById("container");
+
+  container.innerHTML = `<h1>Ciao Bella Me piacere parlare l'Italiano </h1>`;
+});
